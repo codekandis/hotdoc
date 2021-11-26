ade31245197a4862455859600a47436545402064
Christian Ramelow <info@codekandis.net>
2021-11-26 10:52:01 +0100

    Adds the action to show a markdown document.

--- src/Configurations/Plain/routes.php
+++ src/Configurations/Plain/routes.php
@@ -1,7 +1,17 @@
 <?php declare( strict_types = 1 );
 namespace CodeKandis\HotDoc\Configurations\Plain;
 
+use CodeKandis\HotDoc\Frontend\Actions as Frontend;
+use CodeKandis\Tiphy\Http\Requests\Methods;
+
 return [
 	'baseRoute' => '',
-	'routes'    => []
+	'routes'    => [
+		'^/$'                          => [
+			Methods::GET => Frontend\Get\ShowDocumentAction::class
+		],
+		'^/books/(?<book>.+?)(:?/(?<chapter>.+))$' => [
+			Methods::GET => Frontend\Get\ShowDocumentAction::class
+		]
+	]
 ];
---
+++ src/Frontend/Actions/Get/ShowDocumentAction.php
@@ -0,0 +1,70 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\HotDoc\Frontend\Actions\Get;
+
+use CodeKandis\HotDoc\Environment\Entities\Collections\BookEntityCollectionInterface;
+use CodeKandis\HotDoc\Environment\Io\BooksDirectoryReader;
+use CodeKandis\HotDoc\Frontend\Actions\AbstractAction;
+use CodeKandis\Tiphy\Http\Responses\HtmlTemplateResponder;
+use CodeKandis\Tiphy\Http\Responses\StatusCodes;
+use ReflectionException;
+use function str_replace;
+use function strpos;
+
+/**
+ * Represents the action to show a document.
+ * @package codekandis/hotdoc
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class ShowDocumentAction extends AbstractAction
+{
+	public function execute(): void
+	{
+		$inputData        = $this->getInputData();
+		$requestedBook    = $inputData[ 'book' ];
+		$requestedChapter = $inputData[ 'chapter' ];
+
+		$sanitizedRequestedChapter = $requestedChapter;
+		while ( false !== strpos( '..', $sanitizedRequestedChapter ) )
+		{
+			$sanitizedRequestedChapter = str_replace( '..', '', $sanitizedRequestedChapter );
+		}
+
+		$documentPath = sprintf(
+			'%s/%s/%s',
+			$this->getFrontendConfigurationRegistry()
+				 ->getBooksConfiguration()
+				 ->getBooksPath(),
+			$requestedBook,
+			$sanitizedRequestedChapter
+		);
+
+		( new HtmlTemplateResponder(
+			$this->getFrontendConfigurationRegistry()
+				 ->getTemplateRendererConfiguration(),
+			StatusCodes::OK,
+			[
+				'books'        => $this->readBooks(),
+				'documentPath' => $documentPath
+			],
+			null,
+			'markdown.phtml'
+		) )
+			->respond();
+	}
+
+	/**
+	 * Reads the known books.
+	 * @return BookEntityCollectionInterface The known books.
+	 * @throws ReflectionException An error occured during an entity creation.
+	 */
+	private function readBooks(): BookEntityCollectionInterface
+	{
+		return ( new BooksDirectoryReader(
+			$this
+				->getFrontendConfigurationRegistry()
+				->getBooksConfiguration()
+				->getBooksPath()
+		) )
+			->read();
+	}
+}
