9ee1018004e44e8efca275984c47c10f59c12f51
Christian Ramelow <info@codekandis.net>
2021-11-28 13:10:03 +0100

    Adds a bool to customized string bi-directional converter.

---
+++ src/Environment/Converters/InvalidCustomizedStringsException.php
@@ -0,0 +1,13 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\HotDoc\Environment\Converters;
+
+use RuntimeException;
+
+/**
+ * Represents an exception thrown if customized string are invalid.
+ * @package codekandis/hotdoc
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class InvalidCustomizedStringsException extends RuntimeException
+{
+}
