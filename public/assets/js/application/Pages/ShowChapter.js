e705bda259a40ea91d80bd1c4a574ad72180f1cd
Christian Ramelow <info@codekandis.net>
2021-11-29 09:22:10 +0100

    Adds syntax highlighting.

--- public/assets/js/application/Pages/ShowChapter.js
+++ public/assets/js/application/Pages/ShowChapter.js
@@ -1,9 +1,11 @@
 'use strict';
 
 import DomDocument from '../../library/Dom/DomDocument.js';
+import SyntaxHighlighter from '../CodeBlockDecorators/SyntaxHighlighter.js';
 
 DomDocument.load(
 	( event ) =>
 	{
+		( new SyntaxHighlighter() ).decorate();
 	}
 );
